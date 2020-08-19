0e85082456b74df7028ec078decdcd546e3650c8
faspeeencina <fabian.aspeeencina@studio.unibo.it>
2020-08-19 18:22:06 +0200

    add new control for reduce, avoiding a brewery with beers without 50 reviews

--- src/main/scala/spark/SessionSpark.scala
+++ src/main/scala/spark/SessionSpark.scala
@@ -6,12 +6,12 @@ import org.apache.spark.sql.SparkSession
 abstract class SessionSpark extends Serializable {
 
   def readFile(sparkSession: SparkSession): (RDD[String], RDD[String], RDD[String]) ={
-    val beers = sparkSession.sparkContext.textFile("faspeeencina/datasets/input/project/file/beers.csv")
-    val reviews = sparkSession.sparkContext.textFile("faspeeencina/datasets/input/project/file/reviews.csv")
-    val breweries = sparkSession.sparkContext.textFile("faspeeencina/datasets/input/project/file/breweries.csv")
-//    val beers = sparkSession.sparkContext.textFile("giovannim/dataset/input/datasetprogetto/beers.csv")
-//    val reviews = sparkSession.sparkContext.textFile("giovannim/dataset/input/datasetprogetto/reviews.csv")
-//    val breweries = sparkSession.sparkContext.textFile("giovannim/dataset/input/datasetprogetto/breweries.csv")
+//    val beers = sparkSession.sparkContext.textFile("faspeeencina/datasets/input/project/file/beers.csv")
+//    val reviews = sparkSession.sparkContext.textFile("faspeeencina/datasets/input/project/file/reviews.csv")
+//    val breweries = sparkSession.sparkContext.textFile("faspeeencina/datasets/input/project/file/breweries.csv")
+    val beers = sparkSession.sparkContext.textFile("giovannim/dataset/input/datasetprogetto/beers.csv")
+    val reviews = sparkSession.sparkContext.textFile("giovannim/dataset/input/datasetprogetto/reviews.csv")
+    val breweries = sparkSession.sparkContext.textFile("giovannim/dataset/input/datasetprogetto/breweries.csv")
     (beers,reviews,breweries)
   }
 }
